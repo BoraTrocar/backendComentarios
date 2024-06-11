ae1bec1c63a4f566a9355e62dd5dba1520d2e128
Ricardo Santos Ferreira <ricardo.santos.ferreira29@gmail.com>
2024-06-11 12:43:17 -0300

    Adicionado o prefixo /apicomentarios na url, para que o ngnix reconheça o backend

--- src/main/java/br/edu/fateccotia/boraTrocarComentarios/controller/ComentarioController.java
+++ src/main/java/br/edu/fateccotia/boraTrocarComentarios/controller/ComentarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/apicomentarios")
 public class ComentarioController {
     @Autowired
     ComentarioService comentarioService;
